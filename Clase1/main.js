044d64e69f9593002368d8d9146e835d00ace6cf
Saul Hernandez <saul.hernandez.ipn@gmail.com>
2025-02-04 20:20:16 -0600

    first commit

---
+++ Clase1/main.js
@@ -0,0 +1,20 @@
+console.log("Hola mundo");
+
+let nombre = "Saul";
+let edad = 25;
+let mayoriaEdad = true;
+let saldo = 62003;
+let cuenta = null;
+let id = Symbol("id");
+let numeroGrande = 12461897698162748612386189n;
+console.log(typeof nombre);
+
+let numeros = [1,3,321,4,13,45];
+let componentes = ["asda",23,true,null];
+console.log(numeros[0]);
+
+let suma = function suma(a,b){return a+b}
+console.log(suma(213,12));
+
+let fecha = new Date();
+console.log(fecha);
